d2289efb50edbaf1a6258561a5516f5b6bcc574c
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-04 11:00:19 -0700

    Oddish or evenish (challenge 3) complete

--- sandbox.js
+++ sandbox.js
@@ -16,3 +16,17 @@ function titleCase(sentence) {
   return mappedSentence.join(' ');
 }
 console.log(titleCase('alchemy ROCKS goLD'));
+
+// ODDISH OR EVENISH
+
+function oddishOrEvenish(number) {
+  const stringArray = number.toString().split('');
+  const numArray = stringArray.map(Number);
+  const sum = numArray.reduce(
+    (prevValue, currentValue) => prevValue + currentValue,
+    0
+  );
+  return sum % 2 === 0 ? 'Evenish' : 'Oddish';
+}
+
+console.log(oddishOrEvenish(121));
