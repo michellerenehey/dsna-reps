989c149fb0a5cada4cbd424486afd20152b6d412
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-04 12:30:28 -0700

    Fizbuzz (challenge 5) complete

--- sandbox.js
+++ sandbox.js
@@ -46,3 +46,22 @@ function at(arr, index) {
 }
 // `['a', 'b', 'c', 'd', 'e']`, `1` should get 'b'
 // `['a', 'b', 'c', 'd', 'e']`, `-2` should get 'd'
+
+// FIZZ BUZZ
+function fizzBuzz(number) {
+  const result = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push('FizzBuzz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+console.log(fizzBuzz(16));
